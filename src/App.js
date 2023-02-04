50e5ed44a9074fe7b7b21f798c11831afeda04d6
tohidbinazam <tohidbinazamsunny1@gmail.com>
2023-02-05 00:42:53 +0600

    Upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Checkbox from "./pages/Checkbox/Checkbox";
+// import Upload from "./pages/Upload/Upload";
+
+function App() {
+  return (
+    <>
+      <Checkbox />
+    </>
+  );
+}
+
+export default App;
